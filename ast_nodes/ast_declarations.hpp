d3b399fca2a7a0dcddbb39fc4baa52aaacc77269
Joff 16.04 <jrp2018@ic.ac.uk>
2020-02-28 16:35:51 +0000

    Added most types, bar enum/struct and their fields

--- ast_nodes/ast_declarations.hpp
+++ ast_nodes/ast_declarations.hpp
@@ -1,7 +1,8 @@
 #ifndef AST_DECLS
 #define AST_DECLS
 
-
+//vargs?
+//asbtract declarators?
 
 
 #endif
---
+++ ast_nodes/ast_types.hpp
@@ -0,0 +1,68 @@
+#ifndef AST_TYPE
+#define AST_TYPE
+
+class Type{
+
+};
+
+//---------------------------
+
+class Void : public Type{
+
+};
+
+class Int : public Type{
+
+};
+
+class Char : public Type{
+
+};
+
+class Double : public Type{
+
+};
+
+class Float : public Type{
+
+};
+
+class UserDefinedType : public Type{
+
+};
+
+//---------------------------
+//structs are difficult: TODO!
+class TaggedStructType : public Type{
+//fields accessible inside instances of struct
+};
+
+class StructFieldType : public Type{
+
+};
+
+
+//---------------------------
+
+class IncompleteArrayType : public Type{
+//no size specified
+};
+
+class ArrayType : public Type{
+//size specified
+};
+
+//---------------------------
+
+class FunctionType : public Type{
+//return type
+};
+
+//---------------------------
+
+class PointerType : public Type{
+//pointer to a type
+};
+
+
+#endif
