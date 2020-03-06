ad6840221f3e23723cef62249b82ef86f4173fae
Joff 16.04 <jrp2018@ic.ac.uk>
2020-03-06 15:23:51 +0000

    Including in one file has stopped the huge wall of errors

---
+++ ast_nodes/ast_allnodes.cpp
@@ -0,0 +1,35 @@
+#include "ast_allnodes.hpp"
+
+void ArgExprList::AppendArgExpression(Expression* ArgExpr){
+    Args.push_back(ArgExpr);
+}
+
+/*static*/
+PrefixExpr* PrefixExpr::DecodeUnaryOp(std::string* yytext, Expression* _RHS){
+    std::string op = *yytext;
+    if(op == "*") return new UnaryDerefOperator(_RHS);
+    else if(op == "&") return new UnaryAddressOperator(_RHS);
+    else if(op == "+") return new UnaryPlusOperator(_RHS);  
+    else if(op == "-") return new UnaryNegOperator(_RHS);
+    else if(op == "~") return new UnaryBitwiseNotOperator(_RHS);
+    else if(op == "!") return new UnaryLogicalNotOperator(_RHS);
+    else return NULL;
+    
+}/*static*/
+GenericAssignExpr* GenericAssignExpr::DecodeAssignOp(Expression* LHS, std::string* yytext, Expression* RHS)
+{
+    std::string op = *yytext;
+    if(op == "=") return new AssignmentExpression(LHS,RHS);
+    else if(op == "*=") return new MulAssignment(LHS, RHS);
+    else if(op == "/=") return new DivAssignment(LHS,RHS);
+    else if(op == "%=") return new ModAssignment(LHS,RHS);
+    else if(op == "+=") return new AddAssignment(LHS,RHS);
+    else if(op == "-=") return new SubAssignment(LHS,RHS);
+    else if(op == "<<=") return new ShiftLeftAssignment(LHS,RHS);
+    else if(op == ">>=") return new ShiftRightAssignment(LHS,RHS);
+    else if(op == "&=") return new BitwiseANDAssignment(LHS,RHS);
+    else if(op == "^=") return new BitwiseXORAssignment(LHS,RHS);
+    else if(op == "|=") return new BitwiseORAssignment(LHS,RHS);
+    else return NULL;
+}
+
