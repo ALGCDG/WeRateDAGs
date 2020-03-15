8d3239baae6a4f707fcf5cc727a9d20e738ece90
Joff 16.04 <jrp2018@ic.ac.uk>
2020-03-15 03:29:57 +0000

    might have to use double dispatch to solve

--- test/C90 tests/test.cpp
+++ test/C90 tests/test.cpp
@@ -3,16 +3,26 @@
 #include <iostream>
 
 class A{
+    public:
     int a;
-    virtual void f() = 0;
+    virtual A* f() = 0;
 };
 
 class B : public A{
+    public:
     int b;
-    void f(){}
+    B* f() override{ return this; }
+};
+
+class C : public A{
+public:
+    C* f() override { return this; }
 };
 
 void func(A*){
+    std::cout << -1;
+}
+void func(C*){
     std::cout << 0;
 }
 
@@ -20,10 +30,12 @@ void func(B*){
     std::cout << 1;
 }
 int main(){
-    A *a, *b;
-    B *c, *d;
+    A * a = new C;
+    A * b = new C;
+    B * c = new B;
+    B * d = new B;
     std::vector<A*> vec = {a,b,c,d};
     for (auto i: vec){
-        func(i);
+        func(i->f());
     }
 }
