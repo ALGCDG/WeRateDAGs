0166cd2ebe1e3eab675f2f47f2e3cd9e603fc7e2
archie <ac11018@ic.ac.uk>
2020-03-30 16:32:07 +0100

    merging for pointer fix

--- compiler_tests/strings/puts_driver.c
+++ compiler_tests/strings/puts_driver.c
@@ -1,9 +1,15 @@
 #include <string.h>
-
+#include <stdio.h>
 int ok;
 
 void fakeputs(char *x)
 {
+    while(*x)
+	{
+    		printf("%c", *x);
+		x++;
+	}
+	printf("\n");
     ok=!strcmp(x,"wibble");
 }
 
